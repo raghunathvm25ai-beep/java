755e471da4b74caa82c37ec40bf1f17f1b6d1a7f
Ragunath <raghunathvm25ai@vetias.ac.in>
2026-06-06 11:59:29 +0530

    new java

---
+++ src/hello.java
@@ -0,0 +1,31 @@
+
+    void main (){
+    int a=1;
+    int b=5;
+    int c=3;
+    int d=4;
+    int f=a+b-(c*d) ;
+        System.out.print("hello world \n");
+        System.out.print(f);
+        System.out.print("\n");
+        String g="billaram";
+        String h="cooker=15";
+        String i="diesel=~";
+        String j="korangu=1";
+        String k="tyre=2";
+        String l="rto=1";
+        String m="delivary=correct time";
+        String n="favorite dialogue=nammatha";
+        String o="favorite coffee thul=bru";
+        String p="loan=40l";
+        System.out.print(g);
+        System.out.print(h);
+        System.out.print(i);
+        System.out.print(j);
+        System.out.print(k);
+        System.out.print(l);
+        System.out.print(m);
+        System.out.print(n);
+        System.out.print(o);
+        System.out.print(p);
+    }
