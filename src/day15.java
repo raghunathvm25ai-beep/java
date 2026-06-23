445572338b384a867eabf726fd65becac4cb216c
madhupratikaar25ai-dot <madhupratikaar25ai@vetias.ac.in>
2026-06-23 10:54:05 +0530

    day 15

---
+++ src/day15.java
@@ -0,0 +1,20 @@
+public class day15 {
+    static void main() throws InterruptedException {
+        Runnable chefTask = () -> {
+            String[] items = {"Burger", "Momos", "Oreo milkshake"};
+            for (int i = 0; i < items.length; i++) {
+                System.out.println("Cooking Item:" + items[i]);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        };
+        Thread chefThread = new Thread(chefTask, "chef_thread");
+        chefThread.start();
+        chefThread.join();
+
+    }
+}
